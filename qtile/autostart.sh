2b4a8902224a096c6cbb5c9625ef661e8f3456b7
Eduardo Rosário <eduardojfrosario@gmail.com>
2025-04-09 17:56:27 +0100

    changes

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -13,7 +13,7 @@ nm-applet &
 
 blueman-applet &
 
-/usr/lib/polkit-kde-authentication-agent-1 &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 # Run the random wallpaper script
 python ~/.config/qtile/random_wallpaper.py &
 # nitrogen --set-scaled ~/Pictures/red_and_grey.jpg
