8162e0dbfc5050add87347cf5eaa7d3495a67e3a
eduardojfrosario <eduardojfrosario@gmail.com>
2025-08-10 02:02:37 +0100

    fix: conform working for desired languages

--- home/hosts/alpha/nvim/default.nix
+++ home/hosts/alpha/nvim/default.nix
@@ -1,74 +1,87 @@
-{ config, pkgs, inputs, ... }:
 {
-    imports = [
-        inputs.nvf.homeManagerModules.default
-    ];
-    programs.nvf = {
-        enable = true;
-        settings = {
-            vim = {
-                # lsp
-                lsp.enable = true;
-                snippets.luasnip.enable = true;
-                statusline.lualine.enable = true;
-                autocomplete.nvim-cmp.enable = true;
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    inputs.nvf.homeManagerModules.default
+  ];
+  programs.nvf = {
+    enable = true;
+    settings = {
+      vim = {
+        # lsp
+        lsp.enable = true;
+        snippets.luasnip.enable = true;
+        statusline.lualine.enable = true;
+        autocomplete.nvim-cmp.enable = true;
 
-                # navigation
-                telescope.enable = true;
-                navigation.harpoon.enable = true;
+        # navigation
+        telescope.enable = true;
+        navigation.harpoon.enable = true;
 
-                # utils
-                utility.undotree.enable = true;
-                autopairs.nvim-autopairs.enable = true;
+        # utils
+        utility.undotree.enable = true;
+        autopairs.nvim-autopairs.enable = true;
 
-                visuals = {
-                    rainbow-delimiters.enable = true;
-                    nvim-cursorline = {
-                        enable = true;
-                        setupOpts.cursorline.enable = true;
-                    };
-                };
-
-                languages = {
-                    enableTreesitter = true;
-
-                    nix.enable = true;
-                    python.enable = true;
-                    ts.enable = true;
-                    rust.enable = true;
-                    clang.enable = true;
-                    markdown.enable = true;
-                };
-
-                formatter.conform-nvim = {
-                    enable = true;
-                    setupOpts.formatters_by_ft = {
-                        lua = [ "stylua" ];
-                        nix = [ "nixpkgs-fmt" ];
-                        c = [ "clang-format" ];
-                        python = [ "black" ];
-                        javascript = [ "prettierd" ];
-                        typescript = [ "prettierd" ];
-                        javascriptreact = [ "prettierd" ];
-                        typescriptreact = [ "prettierd" ];
-                        html = [ "prettierd" ];
-                    };
-                };
-
-                extraLuaFiles = [
-                    ./remap.lua
-                    ./set.lua
-                ];
-
-                extraPlugins = with pkgs.vimPlugins; {
-                    markdown-preview = {
-                        package = markdown-preview-nvim;
-                        setup = ''
-                          vim.g.mkdp_filetypes = { "markdown" }
-                        '';
-                    };
-                };
-            };
+        visuals = {
+          rainbow-delimiters.enable = true;
+          nvim-cursorline = {
+            enable = true;
+            setupOpts.cursorline.enable = true;
+          };
         };
+
+        languages = {
+          enableTreesitter = true;
+
+          nix.enable = true;
+          python.enable = true;
+          ts.enable = true;
+          rust.enable = true;
+          clang.enable = true;
+          markdown.enable = true;
+        };
+
+        formatter.conform-nvim = {
+          enable = true;
+          setupOpts.formatters_by_ft = {
+            lua = [ "stylua" ];
+            nix = [ "nixpkgs-fmt" ];
+            c = [ "clang-format" ];
+            python = [ "black" ];
+            javascript = [ "prettierd" ];
+            typescript = [ "prettierd" ];
+            javascriptreact = [ "prettierd" ];
+            typescriptreact = [ "prettierd" ];
+            html = [ "prettierd" ];
+          };
+        };
+
+        extraLuaFiles = [
+          ./remap.lua
+          ./set.lua
+        ];
+
+        extraPlugins = with pkgs.vimPlugins; {
+          markdown-preview = {
+            package = markdown-preview-nvim;
+            setup = ''
+              vim.g.mkdp_filetypes = { "markdown" }
+            '';
+          };
+        };
+      };
     };
+  };
+  home.packages = with pkgs; [
+    stylua
+    black
+    clang-tools
+    prettierd
+    nixpkgs-fmt
+  ];
+
 }
