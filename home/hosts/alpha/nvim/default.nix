a585c22e25680c7d2cc8a9a1e1c342c9ee58620d
eduardojfrosario <eduardojfrosario@gmail.com>
2025-08-10 01:45:50 +0100

    fix: conform and cursorline working

--- home/hosts/alpha/nvim/default.nix
+++ home/hosts/alpha/nvim/default.nix
@@ -7,14 +7,28 @@
         enable = true;
         settings = {
             vim = {
+                # lsp
                 lsp.enable = true;
+                snippets.luasnip.enable = true;
                 statusline.lualine.enable = true;
+                autocomplete.nvim-cmp.enable = true;
+
+                # navigation
                 telescope.enable = true;
                 navigation.harpoon.enable = true;
-                visuals.rainbow-delimiters.enable = true;
+
+                # utils
                 utility.undotree.enable = true;
-                autocomplete.nvim-cmp.enable = true;
                 autopairs.nvim-autopairs.enable = true;
+
+                visuals = {
+                    rainbow-delimiters.enable = true;
+                    nvim-cursorline = {
+                        enable = true;
+                        setupOpts.cursorline.enable = true;
+                    };
+                };
+
                 languages = {
                     enableTreesitter = true;
 
@@ -23,14 +37,29 @@
                     ts.enable = true;
                     rust.enable = true;
                     clang.enable = true;
-                    java.enable = true;
                     markdown.enable = true;
                 };
-                formatter.conform-nvim.enable = true;
+
+                formatter.conform-nvim = {
+                    enable = true;
+                    setupOpts.formatters_by_ft = {
+                        lua = [ "stylua" ];
+                        nix = [ "nixpkgs-fmt" ];
+                        c = [ "clang-format" ];
+                        python = [ "black" ];
+                        javascript = [ "prettierd" ];
+                        typescript = [ "prettierd" ];
+                        javascriptreact = [ "prettierd" ];
+                        typescriptreact = [ "prettierd" ];
+                        html = [ "prettierd" ];
+                    };
+                };
+
                 extraLuaFiles = [
                     ./remap.lua
                     ./set.lua
                 ];
+
                 extraPlugins = with pkgs.vimPlugins; {
                     markdown-preview = {
                         package = markdown-preview-nvim;
