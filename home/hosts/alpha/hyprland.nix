72b7ceef9c97706b461304b695e497f87da37a66
eduardojfrosario <eduardojfrosario@gmail.com>
2025-10-28 12:15:56 +0000

    change default explorer back to nemo

--- home/hosts/alpha/hyprland.nix
+++ home/hosts/alpha/hyprland.nix
@@ -13,7 +13,7 @@
     settings = {
       "$mainMod" = "SUPER";
       "$terminal" = "ghostty";
-      "$fileManager" = "nautilus";
+      "$fileManager" = "nemo";
       "$menu" = "rofi -show drun -show-icons";
       "$resize_px" = "20";
 
